7977bb2ed157831d2b4adfaa3ea1223ea850442f
Dylan Anthony <dbanty@users.noreply.github.com>
2025-01-03 15:13:08 -0700

    fix: Clear the output directory before dumping

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 # shellcheck disable=SC2086
-mydumper --host "$MYSQL_HOST" --user $MYSQL_USER --password $MYSQL_PASSWORD --port $MYSQL_PORT --database $MYSQL_DATABASE -C -c -o backup
+mydumper --host "$MYSQL_HOST" --user $MYSQL_USER --password $MYSQL_PASSWORD --port $MYSQL_PORT --database $MYSQL_DATABASE -C -c --clear -o backup
 rclone config touch
 cat <<EOF > ~/.config/rclone/rclone.conf
 [remote]
